d54cc60c4ab59c59c30fbe30ef50e520454be757
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-03-12 14:31:33 +0530

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+// whenever u have to use middleware or to set config that time we use app.use(cors())
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+app.use(express.json({limit : "10kb"}))
+app.use(express.urlencoded({extended : true, limit : "10kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+export { app }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ dotenv.config({
     path : './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error Occur.", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGO db connection failed !!!", error);
+})
 
 
 
